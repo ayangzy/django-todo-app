d517184461cb66d25724f2f85f9316aff23d2ec1
Ayange Felix <ayangefelix8@gmail.com>
2022-01-22 23:03:36 +0100

    handle error and add login page

--- TodoPproject/urls.py
+++ TodoPproject/urls.py
@@ -22,4 +22,5 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User;
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 
+
 def signupuser(request):
     if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form': UserCreationForm})
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
-        #create new user here
+        # create new user here
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], request.POST['password1'])
-            user.save()
-            return render(request, 'todo/signupuser.html')
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], request.POST['password1'])
+                user.save()
+                login(request, user)
+                #return render(request, 'todo/login.html')
+                return redirect('loginuser')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': "The username has already been taken, please try another one"})
         else:
-            return render(request, 'password does not match')
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': "Password does not match"})
+
+def loginuser(request):
+    return render(request, 'todo/login.html')
