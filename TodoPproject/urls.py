c1ba92adea0c9b00510ad652bcdbe825803fa16b
Ayange Felix <ayangefelix8@gmail.com>
2022-02-07 13:28:26 +0100

    add todo completed and delet logic

--- TodoPproject/urls.py
+++ TodoPproject/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),  
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'), 
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),  
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from xml.etree.ElementTree import Comment
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from todo.models import Todo
 from .forms import TodoForm
@@ -77,14 +78,28 @@ def viewtodo(request, todo_pk):
         return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
     else:
         try:
-            form = TodoForm(request.POST,instance=todo)
+            form = TodoForm(request.POST, instance=todo)
             form.save()
             return redirect('currenttodo')
-        except:ValueError
+        except: ValueError
         return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad request'})
+
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodo')
+    
+    
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodo')
         
-
-
+    
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
